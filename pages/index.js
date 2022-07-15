14633fbd061d2a63641ac6c161d6f2e2a7d1b7e3
Osc2405 <orosero2405@gmail.com>
2022-07-14 20:16:34 -0500

    Eslint configuration

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
-import {useRouter} from 'next/router'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Oscar Portfolio</title>
-        <meta name="description" content="Portfolio as developer and data scientist" />
+        <meta
+          name="description"
+          content="Portfolio as developer and data scientist"
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className="main">
         <h1>Portfolio</h1>
-        <Link href='/projects'><a>Projects</a></Link>
+        <Link href="/projects">
+          <a>Projects</a>
+        </Link>
       </main>
     </div>
-  )
+  );
 }
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,12 +1,12 @@
 import Link from 'next/link'
 
-export default function Projects(){
-    return (
+export default function Projects () {
+  return (
         <>
         <main className="main">
             <h1>This are my projects</h1>
             <Link href="/"><a>Regresar al Home</a></Link>
         </main>
         </>
-        )
+  )
 }
